11bc31af52f0b468121791c62c2cd321878cc14f
Jacob Young <jacobly0@users.noreply.github.com>
2016-11-25 14:34:34 -0500

    Named some more usb things.

--- core/usb/usb.h
+++ core/usb/usb.h
@@ -20,6 +20,12 @@ extern usb_state_t usb;
 eZ80portrange_t init_usb(void);
 void usb_reset(void);
 
+void usb_host_int(uint8_t);
+void usb_otg_int(uint16_t);
+void usb_grp0_int(uint8_t);
+void usb_grp1_int(uint32_t);
+void usb_grp2_int(uint16_t);
+
 /* Save/Restore */
 #include "../emu.h"
 bool usb_restore(const emu_image*);
